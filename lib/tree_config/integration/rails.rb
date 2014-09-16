837ccad92484d33c0e6393dafb34e83ca48da240
Huy Do <doxuanhuy@gmail.com>
2014-09-17 00:29:55 +0900

    Update rails.rb

--- lib/tree_config/integration/rails.rb
+++ lib/tree_config/integration/rails.rb
@@ -2,12 +2,12 @@ module TreeConfig
   module Rails
     if defined?(Rails::Railtie)
       class Railtie < Rails::Railtie
-        initializer :load_custom_hekk_config, :before => :load_environment_config, :group => :all do
+        initializer :load_custom_tree_config, :before => :load_environment_config, :group => :all do
           initializer = Rails.root.join("config", "initializer", "hekk_config.rb")
           require initializer if File.exists?(initializer)
         end
 
-        initializer :load_hekk_config_settings, :after => :load_custom_rails_config, :before => :load_environment_config, :group => :all do
+        initializer :load_tree_config_settings, :after => :load_custom_rails_config, :before => :load_environment_config, :group => :all do
           HekkConfig.load(Rails.root.join("config", "settings"))
         end
       end
